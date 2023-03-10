e6d19cf87cd9f2b46b8d58161bdb66ba7231995e
DANIEL SILVA DE LIMA <202108028576@alunos.estacio.br>
2023-03-09 21:12:57 -0300

    second commit

---
+++ js-basics/01-Basics.js
@@ -0,0 +1,21 @@
+var nome1 = 'JOAO'; //string
+nome1 = 'MATEUS'; //string
+nome1 = 84564654; //number
+
+
+nome2 = 'FELIPE'
+
+const salario = 1300 //number
+salario = 1500;
+
+const pincel = {
+	cor:'azul',
+	tipoDePonta:1,
+	possuiTinta:true,
+	fabricante: 'Pilot'
+}
+
+const caixa = {
+	quantidade:12,
+	itens:[pincel, pincel, pincel]
+}
